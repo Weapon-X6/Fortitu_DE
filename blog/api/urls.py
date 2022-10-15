69fa118db1ba576e5a9d417183d6d4c276799c96
Eduardo Hernández <argueta_eduardo@yahoo.de>
2022-10-15 22:49:46 +0000

    Add ViewSets and routers for Tag and Post

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,16 +6,12 @@ from drf_yasg.views import get_schema_view
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
-from blog.api.views import PostList, PostDetail, UserDetail
-
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
-    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
-    path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),  
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
@@ -48,4 +44,12 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+]
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
+urlpatterns += [
+    path("", include(router.urls)),
 ]
