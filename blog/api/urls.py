698bdcc764e6149987b95f73eeb6077ae7c30ace
Eduardo Hernández <argueta_eduardo@yahoo.de>
2022-10-16 23:01:10 +0000

    Add filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -52,4 +52,9 @@ router.register("posts", PostViewSet)
 
 urlpatterns += [
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
