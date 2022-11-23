3e302d872284f425c20ced8276bfb3dbaac47f68
Eduardo Hernández <argueta_eduardo@yahoo.de>
2022-11-23 17:38:26 +0000

    Adds some playground Js classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,65 @@
-const theNumber = 2
-let yourName = 'Ben'
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
 
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
 }
 
-alert(yourName)
+class DelayedGreeter extends Greeter {
+  delay = 1000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+  
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+  
+  greet2(){
+    setTimeout(
+      function() {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    ) 
+  }  
+
+  getGreeting2(){ 
+    return 'aus'
+  }
+ }
+
+function getGreeting(){ 
+  console.info('greeting')
+}
+
+ function showGreeting(){ 
+  console.info('show')
+}
+
+const g = new Greeter('Patchy')
+g.greet()
+const g2 = new DelayedGreeter('son')
+g2.greet()
