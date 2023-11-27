967704f24312f62b7db91e05c7acbc8990daddc5
waffe_x20 <argueta_eduardo@yahoo.de>
2023-11-26 21:46:26 -0600

    Remove dotenv from settings

--- blango/settings.py
+++ blango/settings.py
@@ -20,15 +20,12 @@ from configurations import Configuration, values
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
-    DOTENV = BASE_DIR / ".env"
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-    # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = "django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-"
 
-    # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
 
     ALLOWED_HOSTS = values.ListValue(["127.0.0.1", "localhost"])
